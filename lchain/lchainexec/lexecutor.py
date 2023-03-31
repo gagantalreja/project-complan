ea86347e08fdaa3199a62758246c2fff12f97403
Harpahul Singh Bhatia <I0718@IT001664.local>
2023-04-01 04:52:44 +0530

    Langchain #4: new pkl

--- lchain/lchainexec/lexecutor.py
+++ lchain/lchainexec/lexecutor.py
@@ -9,10 +9,11 @@ class LExecutor:
         self.temperature = 0
         self.ai_model = OpenAI(temperature=self.temperature)
         # self.chat_model = ConversationChain(llm=self.ai_model, verbose=False)
-        index = faiss.read_index("resources/docs2.index")
-        with open("resources/faiss_store.pkl", "rb") as f:
+        index = faiss.read_index("resources/docs.index")
+        with open("resources/faiss_store1.pkl", "rb") as f:
             store = pickle.load(f)
         store.index = index
+        print(type(store))
         self.qa_chain = VectorDBQAWithSourcesChain.from_llm(llm=self.ai_model, vectorstore=store)
 
     def lexec(self):
