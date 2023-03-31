ca4cae5f8a20ba1d776819da5851777950a0985e
Harpahul Singh Bhatia <I0718@IT001664.local>
2023-04-01 03:43:07 +0530

    Langchain #3: Prototype 1

--- lchain/lchainexec/lexecutor.py
+++ lchain/lchainexec/lexecutor.py
@@ -1,15 +1,25 @@
 from langchain import OpenAI, ConversationChain
+from langchain.chains import VectorDBQAWithSourcesChain
+import faiss
+import pickle
+
 
 class LExecutor:
     def __init__(self):
         self.temperature = 0
         self.ai_model = OpenAI(temperature=self.temperature)
-        self.chat_model = ConversationChain(llm=self.ai_model, verbose=False)
-
+        # self.chat_model = ConversationChain(llm=self.ai_model, verbose=False)
+        index = faiss.read_index("resources/docs2.index")
+        with open("resources/faiss_store.pkl", "rb") as f:
+            store = pickle.load(f)
+        store.index = index
+        self.qa_chain = VectorDBQAWithSourcesChain.from_llm(llm=self.ai_model, vectorstore=store)
 
     def lexec(self):
         while True:
             text = input()
             if text.lower() == "bye":
                 return
-            print(self.chat_model.predict(input=text))
+            result_response = self.qa_chain({"question": text})
+            print(result_response['answer'])
+            print(result_response['sources'])
